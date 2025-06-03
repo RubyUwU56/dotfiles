617662733dc7655a46f407485edb11974d8b04c4
RubyUwU56 <svenpascalricky@gmail.com>
2025-06-03 18:00:00 +0200

    Auto update 2025-06-03 18:00:00

--- dotfiles/config/hypr/scripts/alt_tab_switcher.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-windows=$(hyprctl clients -j | jq -r '.[] | "\(.address) \(.app_id) \(.title)"')
-
-selected=$(echo "$windows" | wofi --dmenu -i -p "Switch to window:" | awk '{print $1}')
-
-if [ -n "$selected" ]; then
-  hyprctl dispatch focuswindow address:$selected
-fi
--- dotfiles/config/waybar/scripts/network_popup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-plasmawindowed org.kde.plasma.networkmanagement
-
-chmod +x ~/.config/waybar/scripts/network_popup.sh
